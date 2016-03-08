2ab6961bb98cc3bbab272d23ee31f190c2c0e124
androidwargames <adwagner89@gmail.com>
2016-03-08 15:24:55 -0600

    Finished fibonacci_last_digit.py

--- ProgAss1/fibonacci_last_digit.py
+++ ProgAss1/fibonacci_last_digit.py
@@ -1,9 +1,15 @@
 # Uses python3
 import sys
 
+
 def get_fibonacci_last_digit(n):
-    # write your code here
-    return 0
+    a = [0,1]
+    if n < 2:
+        return a[n]
+    for i in range(n-1):
+        a.append((a[0]+a[1])%10)
+        del a[0]
+    return a[1]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
