3025c5063abb6cb13521412c17152442cb5704ed
androidwargames <adwagner89@gmail.com>
2016-03-08 16:24:25 -0600

    Finished get_fibonaccihuge.py...hopefully

--- ProgAss1/fibonacci_huge.py
+++ ProgAss1/fibonacci_huge.py
@@ -2,8 +2,21 @@
 import sys
 
 def get_fibonaccihuge(n, m):
-    # write your code here
-    return 0
+    a = [0,1]
+    # standard output
+    if n < 2:
+        return a[n]
+    # make a list of modulos
+    for i in range(2,10000):
+        a.append(a[i-2]+a[i-1])
+    b = [x % m for x in a]
+    # test for repeat cycles
+    y = -1
+    for i in range(2,9999):
+        if b[i:i+2]==[0,1]:
+            y = i
+            break
+    return b[n % y]
 
 if __name__ == '__main__':
     input = sys.stdin.read();
