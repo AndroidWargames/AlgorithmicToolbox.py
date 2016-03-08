48b0e71b20348708a9b89ceb260729bced5929cc
androidwargames <adwagner89@gmail.com>
2016-03-08 15:19:29 -0600

    Finished lcm.py

--- ProgAss1/lcm.py
+++ ProgAss1/lcm.py
@@ -1,9 +1,13 @@
 # Uses python3
 import sys
 
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b,a%b)
+
 def lcm(a, b):
-    #write your code here
-    return a*b
+    return a*b/gcd(a,b)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
