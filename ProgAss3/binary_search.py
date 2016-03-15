601a5e1f5def074955e205aaf845ce158c92b1ce
androidwargames <adwagner89@gmail.com>
2016-03-14 21:36:49 -0500

    Finished binary_search

--- ProgAss3/binary_search.py
+++ ProgAss3/binary_search.py
@@ -3,20 +3,14 @@ import sys
 
 def binary_search(a, x):
     left, right = 0, len(a)
-    while right - left > 0:
-        m = right + left // 2
-        if x = a[m]:
+    while right - left >= 0:
+        m = (right + left) // 2
+        if x == a[m]:
             return m
         if x > a[m]:
             left = m+1
-        else
+        else:
             right = m-1
-    return -1
-
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
     return -1
 
 if __name__ == '__main__':
