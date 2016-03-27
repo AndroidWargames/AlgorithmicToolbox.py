2828574b449fde6d4a4c58ed49b958b7300ec383
androidwargames <adwagner89@gmail.com>
2016-03-27 13:30:40 -0500

    sorting.py updated

--- ProgAss3/majority_element.py
+++ ProgAss3/majority_element.py
@@ -1,18 +1,33 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
+def get_majority_element(a):
+    rem = -1
+    final = a
+    lim = len(final)/2
+    while True:
+        b = []
+        if len(a) % 2 == 1:
+            rem = a[-1]
+        size = len(a)//2
+        if size == 0:
+            break
+        for i in range(size):
+            if a[i] == a[i+size]:
+                b.append(a[i])
+        a = b
+    if rem == -1:
+        return 0
+    j = 0
+    for i in final:
+        if i == rem:
+            j += 1
+    if j > lim:
+        return 1
+    else:
+        return 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
-        print(1)
-    else:
-        print(0)
+    print(get_majority_element(a))
