6b5dfff10151dbb847deca10548ac64287d9848c
androidwargames <adwagner89@gmail.com>
2016-03-31 20:28:00 -0500

    binary algo

--- ProgAss3/points_and_segments.py
+++ ProgAss3/points_and_segments.py
@@ -1,77 +1,75 @@
 # Uses python3
 import sys
 
+
 def fast_count_segments(starts, ends, points):
     cnt = [0] * len(points)
-    m = pair_merge_sort(starts, ends)
-    starts, ends = m
+    starts = merge_sort(starts)
+    ends = merge_sort(ends)
     for i in range(len(points)):
-        f = 0
         p = points[i]
-        s = endseeker(starts,p)
-
-        while f < s:
-            if p <= ends[f]:
-                cnt[i] += 1
-            f += 1
+        s = hi_seek(starts, p)
+        e = lo_seek(ends, p)
+        cnt[i] = len(starts) - s - e
     return cnt
 
-def endseeker(a,x):
-    l = 0
+
+def hi_seek(a,x):
     r = len(a) - 1
-    while l < r:
+    l = 0
+    while True:
+        m = (r + l) // 2
+        if a[m] <= x:
+            l = m + 1
+        else:
+            r = m
+        if r == l:
+            if a[r] < x:
+                r += 1
+            if len(a) != r:
+                if a[r] == x:
+                    r += 1
+            break
+    return len(a) - r
+
+
+def lo_seek(a,x):
+    r = len(a) - 1
+    l = 0
+    while True:
         m = (r + l) // 2
         if a[m] < x:
             l = m + 1
-        elif a[m] > x:
-            r = m - 1
         else:
             r = m
-    if r < 0:
-        return 0
-    if l >= len(a):
-        return len(a)
-    if r == l:
-        if a[r] <= x:
-            return r + 1
-        else:
-            return r
-    elif a[m] > x:
-        return m
-    else:
-        return m + 1
+        if r == l:
+            if a[l] > x:
+                l -= 1
+            if -1 != l:
+                if a[l] == x:
+                    l -= 1
+            break
+    return l + 1
 
 
-
-def pair_merge_sort(a, b):
+def merge_sort(a):
     if len(a) <= 1:
-        return [a, b]
+        return a
     cut = len(a) // 2
-    return pair_merger(pair_merge_sort(a[:cut],
-                                       b[:cut]),
-                       pair_merge_sort(a[cut:],
-                                       b[cut:])
-                       )
+    return merger(merge_sort(a[:cut]), merge_sort(a[cut:]))
 
 
-def pair_merger(a, b):
-    sa, sb, ta, tb = a[0], b[0], a[1], b[1]
-    out = [[], []]
-    while len(sa) > 0 and len(sb) > 0:
-        if sa[0] <= sb[0]:
-            out[0].append(sa[0])
-            out[1].append(ta[0])
-            del sa[0]
-            del ta[0]
+def merger(a, b):
+    out = []
+    while len(a) * len(b) > 0:
+        if a[0] <= b[0]:
+            out.append(a[0])
+            del a[0]
         else:
-            out[0].append(sb[0])
-            out[1].append(tb[0])
-            del sb[0]
-            del tb[0]
-    out[0].extend(sa)
-    out[0].extend(sb)
-    out[1].extend(ta)
-    out[1].extend(tb)
+            out.append(b[0])
+            del b[0]
+    out.extend(a)
+    out.extend(b)
     return out
 
 if __name__ == '__main__':
