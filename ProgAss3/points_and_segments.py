0c9621f1d0b3f231f0575a7a1b82204a5c73f0f8
androidwargames <adwagner89@gmail.com>
2016-03-31 22:05:05 -0500

    CORRECTAMUNDO!!!

--- ProgAss3/points_and_segments.py
+++ ProgAss3/points_and_segments.py
@@ -1,11 +1,12 @@
 # Uses python3
 import sys
-
+import time
+import random
 
 def fast_count_segments(starts, ends, points):
     cnt = [0] * len(points)
-    starts = merge_sort(starts)
-    ends = merge_sort(ends)
+    starts = randomized_quick_sort(starts, 0, len(ends)-1)
+    ends = randomized_quick_sort(ends, 0, len(ends)-1)
     for i in range(len(points)):
         p = points[i]
         s = hi_seek(starts, p)
@@ -14,9 +15,14 @@ def fast_count_segments(starts, ends, points):
     return cnt
 
 
-def hi_seek(a,x):
+def hi_seek(a, x):
     r = len(a) - 1
     l = 0
+    if r == l:
+        if x <= a[0]:
+            return 1
+        else:
+            return 0
     while True:
         m = (r + l) // 2
         if a[m] <= x:
@@ -33,9 +39,14 @@ def hi_seek(a,x):
     return len(a) - r
 
 
-def lo_seek(a,x):
+def lo_seek(a, x):
     r = len(a) - 1
     l = 0
+    if r == l:
+        if x >= a[0]:
+            return 1
+        else:
+            return 0
     while True:
         m = (r + l) // 2
         if a[m] < x:
@@ -71,6 +82,47 @@ def merger(a, b):
     out.extend(a)
     out.extend(b)
     return out
+
+def randomized_quick_sort(a, l, r):
+    if l >= r:
+        return a
+    random.seed(25)
+    k = random.randint(l, r)
+    a[l], a[k] = a[k], a[l]
+    # use partition3
+    m = partition3(a, l, r)
+    randomized_quick_sort(a, l, m[0] - 1)
+    randomized_quick_sort(a, m[1] + 1, r)
+    return a
+
+
+def partition3(a, l, r):
+    x = a[l]
+    j, k = l, l
+    for i in range(l+1, r + 1):
+        if a[i] < x:
+            j += 1
+            k += 1
+            a[i], a[j] = a[j], a[i]
+            if j != k:
+                a[i], a[k] = a[k], a[i]
+        elif a[i] == x:
+            k += 1
+            a[i], a[k] = a[k], a[i]
+    a[l], a[j] = a[j], a[l]
+    return [j, k]
+
+
+def quick_timer(a):
+    z = time.time()
+    randomized_quick_sort(a,0,len(a)-1)
+    return time.time() - z
+
+def merge_timer(a):
+    z = time.time()
+    merge_sort(a)
+    return time.time() - z
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
